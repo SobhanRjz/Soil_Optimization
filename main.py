311ac739c3df3cf181c1f35f926998c055362872
SobhanRjz <sobhan.rajabzadeh@gmail.com>
2025-01-27 20:39:56 +0330

    full implementation of PLAXIS

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ class PlaxisModel:
         
     def create_soil_profile(self):
         """Create soil layers and materials"""
-        self.g_i.borehole(0)
+        borhole = self.g_i.borehole(0)
+        borhole.Head = -20
         self.soillayer = self.g_i.soillayer(20)
         
         # Create soil material
@@ -64,7 +65,7 @@ class PlaxisModel:
         self.plate = self.g_i.platemat()
         self.plate.MaterialType = "Elastic"
         self.plate.Identification = "Plate"
-        self.plate.setproperties("w", 2000000)
+        self.plate.setproperties("w", 2.3)
         self.plate.setproperties("EA1", 2000000)
         self.plate.setproperties("EI", 1667)
         self.plate.setproperties("StructNu", 0.2)
@@ -91,15 +92,77 @@ class PlaxisModel:
         for y in range(-self.step_phase, self.ybot_plate, -self.step_phase):
             geogrid = self.g_i.geogrid((0, y), (-geogrid_length, y - self.step_phase))
             geogrid[2].setproperties("Geogrid.Material", self.geogrid)
+
+        # Create Line Loads
+        load = self.g_i.lineload((0,0), (-self.contour_width / 2 ,0))
+        load[3].qy_start = -15 # Kn / m
+
         
         print("Soil layer created.")
+
         
     def save_and_close(self, project_name="my_plaxis_project.p2dx"):
         """Save project and close connection"""
         save_path = os.path.join(os.getcwd(), project_name)
-        self.s_i.save(save_path)
-        self.s_i.close()
+        self.g_i.save(save_path)
+        self.g_i.close()
         print(f"2D Project '{project_name}' created and saved at '{save_path}'")
+
+    def Automesh(self):
+        self.g_i.gotomesh()
+        mesh = self.g_i.mesh(0.06) # medium
+        print(mesh)
+    
+    def createPhase(self):
+        self.g_i.gotoflow()
+
+        #Activate line load for IntialPhases
+        line_loads = self.g_i.LineLoads
+        initial_phase = self.g_i.Phases[0]
+        self.g_i.activate(line_loads , initial_phase)
+
+
+        soils = self.g_i.Soils
+        lines = self.g_i.Lines
+        geogrid_lines = self.g_i.GeoGrids
+        # Filter lines that are associated with plates
+        # Get all lines in the model
+        lines = self.g_i.Lines
+
+        # Filter lines that are associated with geogrid materials
+        geogrid_lines = [line for line in lines if hasattr(line, "Geogrid")]
+
+        # Create map of soil names to soil objects
+        soil_map = {soil.Name.echo().split(".")[0]: soil for soil in soils}
+        line_map = {line.Name.echo().split(".")[0]: line for line in lines}
+        Geo_map = {Geo.Name.echo().split(".")[0]: Geo for Geo in geogrid_lines}
+        firstIndexGepMap = min(int(name.split('_')[1]) for name in Geo_map.keys())
+
+        for indexS in range(1, int(abs(self.ybot_plate / 2)) + 1): # FIX round
+
+            #create New Phase
+            new_phase = self.g_i.phase(self.g_i.Phases[-1])
+
+            # Deactivate Soils
+            soilName = f"Soil_1_{indexS}"
+            Soil = soil_map[soilName]
+            self.g_i.deactivate(Soil, new_phase)
+
+            # Activate ShotCrete
+            lineName = f"Line_1_{indexS}"
+            ShotCrete = line_map[lineName]
+            self.g_i.activate(ShotCrete, new_phase)
+
+            # Activate Nailing
+            GeoName = f"Line_{firstIndexGepMap + indexS - 1}_1"
+            if GeoName in Geo_map:
+                Nail = Geo_map[GeoName]
+                self.g_i.activate(Nail, new_phase)
+
+        print("")
+    def Run(self):
+        self.g_i.calculate()
+        print("")
 
 def main():
     # Create and run model
@@ -111,6 +174,9 @@ def main():
     model.create_structural_materials()
     model.assign_materials()
     model.create_structures()
+    model.Automesh()
+    model.createPhase()
+    model.Run()
     model.save_and_close()
 
 if __name__ == "__main__":
